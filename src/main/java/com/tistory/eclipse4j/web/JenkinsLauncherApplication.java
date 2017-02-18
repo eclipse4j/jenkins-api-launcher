4b1ad8eb4aff1c412d270d971dfef512b0c44e2a
eclipse4j <eclipse4j@Grissom-PC>
2017-02-19 04:35:39 +0900

    Docker Build

--- src/main/java/com/tistory/eclipse4j/web/JenkinsLauncherApplication.java
+++ src/main/java/com/tistory/eclipse4j/web/JenkinsLauncherApplication.java
@@ -2,13 +2,11 @@ package com.tistory.eclipse4j.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.ActiveProfiles;
 
 /**
- * http://eclipse4j.tistory.com 
+ * http://eclipse4j.tistory.com
  */
 @SpringBootApplication
-@ActiveProfiles(profiles = "production")
 public class JenkinsLauncherApplication {
 
 	public static void main(String[] args) {
